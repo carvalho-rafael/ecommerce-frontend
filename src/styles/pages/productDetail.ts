4e70cd7d38f5fe89ff0f7f946499deaba690e3f6
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-01-04 19:13:09 -0300

    make product detail style more readable

--- src/styles/pages/productDetail.ts
+++ src/styles/pages/productDetail.ts
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 
-
 export const ProductContainer = styled.div`
     display: flex;
     flex-wrap: wrap;
@@ -17,55 +16,6 @@ export const ProductInfo = styled.div`
         font-weight: normal;
     }
 `
-export const AddToCartButton = styled.button`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 200px;
-    height: 50px;
-    padding: .5rem 1rem;
-    background: ${props => props.theme.text};
-    border: none;
-    color: ${props => props.theme.textLight};
-
-    font-size: 18px;
-    font-weight: bold;
-    cursor: pointer;
-
-    &:hover {
-        background: ${props => props.theme.textDark};
-        transition: .2s linear all;
-    }
-
-    span {
-        margin-left: .5rem;
-    }
-`
-export const BuyButton = styled.button`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 200px;
-    height: 50px;
-    padding: .5rem 1rem;
-    margin-bottom: 1rem;
-    background: ${props => props.theme.secondaryDark};
-    border: none;
-    color: ${props => props.theme.textLight};
-
-    font-size: 18px;
-    font-weight: bold;
-    cursor: pointer;
-
-    &:hover {
-        background: ${props => props.theme.textDark};
-        transition: .2s linear all;
-    }
-
-    span {
-        margin-left: .5rem;
-    }
-`
 export const PaymentInfo = styled.div`
     width: 400px;
     padding: 20px;
@@ -78,4 +28,32 @@ export const PaymentInfo = styled.div`
         font-size: 28px;
         font-weight: normal;
     }
+`
+const BaseButton = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 200px;
+    height: 50px;
+    padding: .5rem 1rem;
+    margin-bottom: .5rem;
+    background: ${props => props.theme.text};
+    border: none;
+    border-radius:  5px;
+    color: ${props => props.theme.textLight};
+
+    font-size: 18px;
+    font-weight: bold;
+    cursor: pointer;
+
+    &:hover {
+        background: ${props => props.theme.textDark};
+        transition: .2s linear all;
+    }
+`
+export const AddToCartButton = styled(BaseButton)`
+    background: ${props => props.theme.text};
+`
+export const BuyButton = styled(BaseButton)`
+    background: ${props => props.theme.secondaryDark};
 `
